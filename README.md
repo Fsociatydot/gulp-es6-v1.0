2ca0d576ebe97649be391e647a5dfbad9b3be7d2
Amberits <info@hdsteam.com>
2023-07-02 10:17:46 +0300

    gulp-es6

---
+++ README.md
@@ -0,0 +1,3 @@
+# GULP ES6
+
+## Version 1.0
---
+++ src/js/modules/functions.js
@@ -0,0 +1,15 @@
+export function isWebp() {
+    //проверка поддержки webp
+    function testWebP(callback) {
+        let webP = new Image()
+        webP.onload = webP.onerror = function () {
+            callback(webP.height == 2)
+        }
+        webP.src = "data:image/webp;base64,UklGRkAAAABXRUJQVlA4IDQAAADwAQCdASoBAAEAAQAcJaACdLoB+AAETAAA/vW4f/6aR40jxpHxcP/ugT90CfugT/3NoAAA"
+        //добавления класса _webp или _no-webp для HTML
+        testWebP(function (support) {
+            let className = support === true ? 'webp' : 'no-webp'
+            document.documentElement.classList.add(className)
+        })
+    }
+}
